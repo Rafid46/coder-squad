15b52b97053b859ba53771621386b195a839b51d
Rafid46 <mostafarafid45@gmail.com>
2024-03-31 19:43:50 +0600

    project, folder created

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: "'Poppins', sans-serif",
+        caveat: "'Caveat', cursive",
+        brolimo: ["BrolimoRegular"],
+        grotest: ["ArchivGroteskTrial-RegularTrial"],
+        neue: ["NeueMontreal-Regular"],
+        founderGrotest: ["FoundersGrotesk-Semibold"],
+        diastema: ["Diastema Regular"],
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+};
